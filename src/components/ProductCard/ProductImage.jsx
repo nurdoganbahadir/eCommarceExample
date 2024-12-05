59e9e916f4b8ca2568933b76b370c896cc7df8fd
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-12-05 15:08:16 +0300

    card yapısına slider eklendi

--- src/components/ProductCard/ProductImage.jsx
+++ src/components/ProductCard/ProductImage.jsx
@@ -1,3 +1,105 @@
-export default function ProductImage({ images }) {
-  return <img src={images} alt="" className="w-96" />;
-}
+"use client";
+import { useState } from "react";
+
+const Carousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleIndicatorClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="relative w-full border-2 rounded-md" data-carousel="slide">
+      <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`absolute inset-0 transition-opacity duration-700 ease-in-out ${
+              currentIndex === index ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <img
+              src={image}
+              className="block w-full h-full object-contain"
+              alt={`Slide ${index + 1}`}
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => handleIndicatorClick(index)}
+            className={`w-3 h-3 rounded-full ${
+              currentIndex === index
+                ? "bg-blue-500"
+                : "bg-gray-300 dark:bg-gray-700"
+            }`}
+            aria-current={currentIndex === index}
+            aria-label={`Slide ${index + 1}`}
+          ></button>
+        ))}
+      </div>
+      <button
+        type="button"
+        onClick={handlePrev}
+        className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+      >
+        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full bg-white">
+          <svg
+            className="w-2 h-2 text-gray-800"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 1 1 5l4 4"
+            />
+          </svg>
+          <span className="sr-only">Previous</span>
+        </span>
+      </button>
+      <button
+        type="button"
+        onClick={handleNext}
+        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+      >
+        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full bg-white">
+          <svg
+            className="w-2 h-2 text-gray-800"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 9 4-4-4-4"
+            />
+          </svg>
+          <span className="sr-only">Next</span>
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/components/ProductCard/ProductInfo.jsx
+++ src/components/ProductCard/ProductInfo.jsx
@@ -15,7 +15,7 @@ export default function ProductInfo({
   const discountPrice = (price - (price * discountPercentage) / 100).toFixed(2);
 
   return (
-    <section>
+    <section className="my-12">
       <div>
         <h1 className="font-bold text-lastColor text-2xl">
           {brand}
@@ -91,9 +91,9 @@ export default function ProductInfo({
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
           </svg>
